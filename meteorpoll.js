e54bca5c23cb04c73d864a51ac3c1b9fc6b82df6
Surendran <surendrans86@gmail.com>
2015-10-17 18:10:15 +0530

    Creating simple question with no options

--- meteorpoll.js
+++ meteorpoll.js
@@ -1,19 +1,33 @@
+Questions = new Mongo.Collection('questions')
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    questions: function(){
+      return Questions.find({}, {sort: {createdAt: -1}})
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+Template.body.events({
+    "submit .new-question": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Questions.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
     }
   });
+
 }
 
 if (Meteor.isServer) {
